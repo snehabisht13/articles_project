7e6a93387e0237fc1715ef4876029cd7cde5820f
Sneha <sneha040222@gmail.com>
2026-02-02 13:19:02 +0530

    Added Signup page & functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root 'pages#home'
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
