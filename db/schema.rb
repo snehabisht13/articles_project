0a4ec6058a3e3543bfbabadff247b3781303d25c
Sneha <sneha040222@gmail.com>
2026-02-02 09:38:23 +0530

    Added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_29_061845) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_01_104006) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
-  end 
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.datetime "updated_at", null: false
+    t.string "username"
+  end
 end
