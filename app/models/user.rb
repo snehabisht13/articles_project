9c4df32a9d684b937744c5593bdb5b6b2930508a
Sneha <sneha040222@gmail.com>
2026-02-02 12:25:11 +0530

    Added user id to every article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord 
+    has_many :articles 
     validates :username, presence: true, length: {minimum: 3, maximum: 25} , uniqueness: {case_sensitive: false}
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: {case_sensitive: false}
 end
