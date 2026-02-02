3a33c611a6894e2df02262b6ca4f17d602e83363
Sneha <sneha040222@gmail.com>
2026-02-02 12:03:59 +0530

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord 
-
+    validates :username, presence: true, length: {minimum: 3, maximum: 25} , uniqueness: {case_sensitive: false}
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: {case_sensitive: false}
 end
