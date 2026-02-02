029d5189cd452c05f690cfa113c9400e20940fcb
Sneha <sneha040222@gmail.com>
2026-02-02 12:45:27 +0530

    Added password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord 
+    before_save {self.email = email.downcase}
     has_many :articles 
     validates :username, presence: true, length: {minimum: 3, maximum: 25} , uniqueness: {case_sensitive: false}
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: {case_sensitive: false}
+    has_secure_password
 end
